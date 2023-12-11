114073c205ddb5bbd068e6322529370d86a1b326
Anthony Oteri <anthony.oteri@gmail.com>
2023-12-11 11:05:52 -0500

    Day 9 - Part 2
    Signed-off-by: Anthony Oteri <anthony.oteri@gmail.com>

--- day-09/src/part2.rs
+++ day-09/src/part2.rs
@@ -1,8 +1,46 @@
 use crate::error::AocError;
 
+fn parse(input: &str) -> Vec<Vec<i32>> {
+    input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|num| num.parse().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+fn get_next(input: Vec<i32>) -> i32 {
+    let input = input.clone();
+
+    if input.iter().all(|&v| v == 0) {
+        return 0;
+    }
+
+    let next_vec = input[1..]
+        .iter()
+        .enumerate()
+        .map(|(i, num)| input[i] - num)
+        .collect::<Vec<_>>();
+
+    input.last().unwrap() - get_next(next_vec.clone())
+}
+
+fn get_prev(input: Vec<i32>) -> i32 {
+    let mut input = input.clone();
+    input.reverse();
+    get_next(input)
+}
+
 #[tracing::instrument]
-pub fn process(input: &str) -> miette::Result<u64, AocError> {
-    Ok(0)
+pub fn process(input: &str) -> miette::Result<i64, AocError> {
+    let input = parse(input);
+    Ok(input
+        .iter()
+        .map(|v| get_prev(v.clone()))
+        .map(i64::from)
+        .sum::<i64>())
 }
 
 #[cfg(test)]
@@ -10,9 +48,15 @@ mod tests {
     use super::*;
 
     #[test_log::test]
+    fn test_get_next() {
+        let input = vec![10, 13, 16, 21, 30, 45];
+        assert_eq!(get_prev(input), 5);
+    }
+
+    #[test_log::test]
     fn test_process() -> miette::Result<()> {
         let input = include_str!("../test-input.txt");
-        assert_eq!(0, process(input)?);
+        assert_eq!(2, process(input)?);
         Ok(())
     }
 }
